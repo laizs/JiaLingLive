eaceae393f474a3ddc01f99d5166b315721b469d
laizs <278900273@qq.com>
2020-06-23 11:50:01 +0800

    整合springboot  mybatis sharding-jdbc

--- demo-sharding-jdbc/src/main/java/com/gzzsc/lai/entity/User.java
+++ demo-sharding-jdbc/src/main/java/com/gzzsc/lai/entity/User.java
@@ -1,10 +1,15 @@
 package com.gzzsc.lai.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class User {
+    @ApiModelProperty(value = "用户id",required = true,dataType="int",notes = "用户的唯一id",example = "1")
     private Long id;
-
+    @ApiModelProperty(value = "用户名",dataType="String",notes = "用户名",example = "张三")
     private String username;
-
+    @ApiModelProperty(value = "用户密码",dataType="String",notes = "用户密码",example = "888888")
     private String password;
 
     public Long getId() {
